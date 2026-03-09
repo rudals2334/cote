685551fb8c162ea0010881a2c7c1dd81648ba39a
Lee Gyungmin <rudals831@gmail.com>
2026-03-09 21:56:35 +0900

    first commit

---
+++ Word_Study.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+// 문제
+// 알파벳 대소문자로 된 단어가 주어지면, 이 단어에서 가장 많이 사용된 알파벳이 무엇인지 알아내는 프로그램을 작성하시오. 단, 대문자와 소문자를 구분하지 않는다.
+
+// 입력
+// 첫째 줄에 알파벳 대소문자로 이루어진 단어가 주어진다. 주어지는 단어의 길이는 1,000,000을 넘지 않는다.
+
+// 출력
+// 첫째 줄에 이 단어에서 가장 많이 사용된 알파벳을 대문자로 출력한다. 단, 가장 많이 사용된 알파벳이 여러 개 존재하는 경우에는 ?를 출력한다.
+
+public class Word_Study {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        String str2 = str.toUpperCase();
+        int[] arr = new int[26];
+
+        for(int i = 0; i < str2.length(); i++) {
+            int a = str2.charAt(i) - 'A';
+            arr[a]++;
+        }
+
+        int max = 0;
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] > max){
+                max = arr[i];
+            }
+        }
+
+        int cnt = 0;
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] == max){
+                cnt++;
+            }
+        }
+        if(cnt > 1){
+            System.out.println("?");
+        } else {
+            for(int i = 0; i < arr.length; i++){
+                if(arr[i] == max){
+                    char c = (char)(i + 'A');
+                    System.out.println(c);
+                }
+            }
+        }
+    }
+}
