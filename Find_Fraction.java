c7a47a315cc520eff487987e750c351e971f73b3
Lee Gyungmin <rudals831@gmail.com>
2026-03-14 23:25:50 +0900

    1193 : 분수찾기

---
+++ Find_Fraction.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Find_Fraction {
+// 문제
+// 무한히 큰 배열에 다음과 같이 분수들이 적혀있다.
+
+// 1/1	1/2	1/3	1/4	1/5	…
+// 2/1	2/2	2/3	2/4	…	…
+// 3/1	3/2	3/3	…	…	…
+// 4/1	4/2	…	…	…	…
+// 5/1	…	…	…	…	…
+// …	…	…	…	…	…
+// 이와 같이 나열된 분수들을 1/1 → 1/2 → 2/1 → 3/1 → 2/2 → … 과 같은 지그재그 순서로 차례대로 1번, 2번, 3번, 4번, 5번, … 분수라고 하자.
+
+// X가 주어졌을 때, X번째 분수를 구하는 프로그램을 작성하시오.
+
+// 입력
+// 첫째 줄에 X(1 ≤ X ≤ 10,000,000)가 주어진다.
+
+// 출력
+// 첫째 줄에 분수를 출력한다.   
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+
+        int line = 1;
+        int sum = 0;
+        while(true){
+            sum+=line;
+            if(sum>=x) break;
+            line++;
+        }
+        int k = x-(sum-line);
+
+        if(line%2==1){ //홀수번째 대각선
+            System.out.println((line-k+1)+"/"+k);
+        }else
+            System.out.println(k+"/"+(line-k+1));
+
+    }
+}
