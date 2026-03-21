e9029e46c027560da7902b7ba3975303d4f76834
Lee Gyungmin <rudals831@gmail.com>
2026-03-21 23:23:44 +0900

    2644: 촌수 계산

---
+++ 2026-03-week3/Degree_of_Kinship.java
@@ -0,0 +1,73 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+public class Degree_of_Kinship {
+// 문제
+// 우리 나라는 가족 혹은 친척들 사이의 관계를 촌수라는 단위로 표현하는 독특한 문화를 가지고 있다. 
+// 이러한 촌수는 다음과 같은 방식으로 계산된다. 기본적으로 부모와 자식 사이를 1촌으로 정의하고 이로부터 사람들 간의 촌수를 계산한다. 
+// 예를 들면 나와 아버지, 아버지와 할아버지는 각각 1촌으로 나와 할아버지는 2촌이 되고, 아버지 형제들과 할아버지는 1촌, 나와 아버지 형제들과는 3촌이 된다.
+// 여러 사람들에 대한 부모 자식들 간의 관계가 주어졌을 때, 주어진 두 사람의 촌수를 계산하는 프로그램을 작성하시오.
+
+// 입력
+// 사람들은 1, 2, 3, …, n (1 ≤ n ≤ 100)의 연속된 번호로 각각 표시된다. 
+// 입력 파일의 첫째 줄에는 전체 사람의 수 n이 주어지고, 둘째 줄에는 촌수를 계산해야 하는 서로 다른 두 사람의 번호가 주어진다. 
+// 그리고 셋째 줄에는 부모 자식들 간의 관계의 개수 m이 주어진다. 넷째 줄부터는 부모 자식간의 관계를 나타내는 두 번호 x,y가 각 줄에 나온다. 
+// 이때 앞에 나오는 번호 x는 뒤에 나오는 정수 y의 부모 번호를 나타낸다.
+// 각 사람의 부모는 최대 한 명만 주어진다.
+
+// 출력
+// 입력에서 요구한 두 사람의 촌수를 나타내는 정수를 출력한다. 어떤 경우에는 두 사람의 친척 관계가 전혀 없어 촌수를 계산할 수 없을 때가 있다. 이때에는 -1을 출력해야 한다.
+    public static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+    public static boolean visited[];
+    public static int degree[];
+    public static void main(String[] args)throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+        visited = new boolean[n+1];
+        degree = new int[n+1];
+        degree[x]=0;
+        int m = Integer.parseInt(br.readLine());
+        for(int i = 0; i <= n; i++){
+            graph.add(new ArrayList<Integer>());
+        }
+        for(int i = 0; i < m; i++){
+            StringTokenizer st2 = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st2.nextToken());
+            int b = Integer.parseInt(st2.nextToken());
+            graph.get(a).add(b);
+            graph.get(b).add(a);
+        }
+        bfs(x);
+        if(degree[y] == 0){
+            System.out.println("-1");
+        }else{
+            System.out.println(degree[y]);
+        }
+
+        
+    }
+    public static void bfs(int x){
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(x);
+        visited[x] = true;
+        while (!q.isEmpty()) {
+            int k = q.poll();
+            for(int i = 0; i < graph.get(k).size(); i++){
+                int y = graph.get(k).get(i);
+                if(!visited[y]){
+                    visited[y] = true;
+                    degree[y] = degree[k]+1;
+                    q.offer(y);
+                }
+            }
+
+        }
+        
+    }
+}
