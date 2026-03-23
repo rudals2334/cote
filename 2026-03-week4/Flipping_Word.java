d7a6d740310d9ae4c12a29c11e36ec26a132774a
Lee Gyungmin <rudals831@gmail.com>
2026-03-23 23:26:55 +0900

    17413: 단어 뒤집기2

---
+++ 2026-03-week4/Flipping_Word.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Flipping_Word {
+// 문제
+// 문자열 S가 주어졌을 때, 이 문자열에서 단어만 뒤집으려고 한다.
+// 먼저, 문자열 S는 아래와과 같은 규칙을 지킨다.
+// 알파벳 소문자('a'-'z'), 숫자('0'-'9'), 공백(' '), 특수 문자('<', '>')로만 이루어져 있다.
+// 문자열의 시작과 끝은 공백이 아니다.
+// '<'와 '>'가 문자열에 있는 경우 번갈아가면서 등장하며, '<'이 먼저 등장한다. 또, 두 문자의 개수는 같다.
+// 태그는 '<'로 시작해서 '>'로 끝나는 길이가 3 이상인 부분 문자열이고, '<'와 '>' 사이에는 알파벳 소문자와 공백만 있다. 단어는 알파벳 소문자와 숫자로 이루어진 부분 문자열이고, 연속하는 두 단어는 공백 하나로 구분한다. 태그는 단어가 아니며, 태그와 단어 사이에는 공백이 없다.
+
+// 입력
+// 첫째 줄에 문자열 S가 주어진다. S의 길이는 100,000 이하이다.
+
+// 출력
+// 첫째 줄에 문자열 S의 단어를 뒤집어서 출력한다.    
+    public static void main(String[] args)throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String S = br.readLine();
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i < S.length(); i++){
+            char s = S.charAt(i);
+            if(s=='<'){
+                while (!stack.isEmpty()) {
+                    System.out.print(stack.pop());
+                }
+                while(true){
+                    s = S.charAt(i);
+                    System.out.print(s);
+                    if (s=='>') {
+                        break;
+                    }
+                    i++;
+                }
+            }else if(s==' '){
+                while (!stack.isEmpty()) {
+                    System.out.print(stack.pop());
+                }
+                System.out.print(' ');
+            }else{
+                stack.push(s);
+            }
+
+        }
+        while (!stack.isEmpty()) {
+            System.out.print(stack.pop());
+            
+        }
+    }
+}
