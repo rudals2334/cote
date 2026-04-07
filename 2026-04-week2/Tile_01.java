0daa164f56bf0f6d42ed0c2734bfa2f036c25fde
Lee Gyungmin <rudals831@gmail.com>
2026-04-08 00:04:20 +0900

    1904: 01타일

--- 2026-04-week2/Tile_01.java
+++ 2026-04-week2/Tile_01.java
@@ -18,13 +18,13 @@ public class Tile_01 {
     public static void main(String[] args)throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[] dp = new int[n+1];
+        long[] dp = new long[n+1];
 
         dp[1] = 1;
         if(n>=2) dp[2] = 2;
 
         for(int i = 3; i <= n; i++){
-            dp[i] = dp[i-2] + dp[i-1];
+            dp[i] = (dp[i-2] + dp[i-1]) % 15746;
         }
         System.out.println(dp[n]);
         
