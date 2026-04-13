db0c7960faa05cc933416cf5fa43ab95acf7d381
Lee Gyungmin <rudals831@gmail.com>
2026-04-13 23:44:23 +0900

    10816: 숫자 카드2

---
+++ 2026-04-week3/Number_Card2.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Number_Card2 {
+    public static void main(String[] args)throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        HashMap<Integer, Integer> map = new HashMap<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++){
+            int num = Integer.parseInt(st.nextToken());
+            map.put(num, map.getOrDefault(num,0) + 1);
+        }
+
+        int m = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < m; i++){
+            int compare = Integer.parseInt(st.nextToken());
+            sb.append(map.getOrDefault(compare, 0)).append(" ");
+        }
+
+        System.out.println(sb);
+        
+
+        
+        
+        
+        
+        
+    }
+}
