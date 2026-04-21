313f7e0e1beddfc1703a4612559c87c8f15cf653
Lee Gyungmin <rudals831@gmail.com>
2026-04-21 21:56:38 +0900

    4358: 생태학

---
+++ 2026-04-week4/Ecology.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Ecology {
+    public static void main(String[] args)throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        TreeMap<String, Integer> map = new TreeMap<>();
+
+        String line;
+        int total = 0;
+        while((line = br.readLine()) != null){
+            if(line.isEmpty()) continue;
+
+            map.put(line, map.getOrDefault(line, 0) + 1);
+            total++;
+        }
+
+        for(String key : map.keySet()){
+            double ratio = (map.get(key) * 100.0) / total;
+            System.out.printf("%s %.4f\n", key, ratio);
+        }
+    }
+}
