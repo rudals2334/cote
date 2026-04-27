d8966bcf09d9c667d93060c7e68ecb7514e61a22
Lee Gyungmin <rudals831@gmail.com>
2026-04-27 21:44:45 +0900

    programmers: 가장 큰 수

---
+++ 2026-04-week5~05-week1/가장 큰 수.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+class Solution {
+    public String solution(int[] numbers) {
+        String[] arr = new String[numbers.length];
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = String.valueOf(numbers[i]);
+        }
+        Arrays.sort(arr,(a,b) -> (b+a).compareTo(a+b));
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < arr.length; i++){
+            sb.append(arr[i]);
+        }
+        if(sb.charAt(0)=='0'){
+            return "0";
+        }
+        return sb.toString();
+    }
+}
