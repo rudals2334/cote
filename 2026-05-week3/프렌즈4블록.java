7ac0ff8c3b189f8edc2cf28d827a44c2407f4dbd
Lee Gyungmin <rudals831@gmail.com>
2026-05-13 00:06:00 +0900

    programmers: 프렌즈4블록

--- 2026-05-week3/프렌즈4블록.java
+++ 2026-05-week3/프렌즈4블록.java
@@ -13,25 +13,20 @@ class Solution {
             for(int i = 0; i < m-1; i++){
                 for(int j = 0; j < n-1; j++){
                     char s = arr[i][j];
-
-                    if(s!='.' &&
-                        arr[i + 1][j] == s &&
-                        arr[i][j + 1] == s &&
-                        arr[i + 1][j + 1] == s){
-                            visited[i][j] = true;
-                            visited[i+1][j] = true;
-                            visited[i][j+1] = true;
-                            visited[i+1][j+1] = true;
-                            found = true;
-                    }
+                    if(s != '.' && arr[i+1][j] == s && arr[i][j+1] == s && arr[i+1][j+1] == s) {
+                        visited[i][j] = true;
+                        visited[i+1][j] = true;
+                        visited[i][j+1] = true;
+                        visited[i+1][j+1] = true;
+                        found = true;
+                    }  
                 }
             }
-
             if(!found){
                 break;
             }
 
-            for(int i = 0; i < m; i ++){
+            for(int i = 0; i < m; i++){
                 for(int j = 0; j < n; j++){
                     if(visited[i][j]){
                         arr[i][j] = '.';
@@ -43,18 +38,17 @@ class Solution {
             for(int j = 0; j < n; j++){
                 int bottom = m-1;
                 for(int i = m-1; i >= 0; i--){
-                    if(arr[i][j]!='.'){
+                    if(arr[i][j] != '.'){
                         arr[bottom][j] = arr[i][j];
 
-                        if(bottom!=i){
+                        if(bottom != i){
                             arr[i][j] = '.';
                         }
-                        bottom --;
+                        bottom--;
                     }
                 }
             }
         }
-
         return answer;
     }
 }
