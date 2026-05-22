68dd602105b1551d603173c38a49acd947972e00
rudals2334 <rudals831@gmail.com>
2026-05-22 19:06:17 +0900

    programmers: 최소직사각형

---
+++ 2026-05-week4/최소직사각형.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+class Solution {
+    public int solution(int[][] sizes) {
+        for(int i = 0; i < sizes.length; i++){
+            Arrays.sort(sizes[i]);
+        }
+        int max = 0;
+        int max2 = 0;
+        for(int i = 0; i < sizes.length; i++){
+            if(sizes[i][1] > max){
+                max = sizes[i][1];
+            }
+
+            if(sizes[i][0] > max2){
+                max2 = sizes[i][0];
+            }
+        }
+
+        return max*max2;
+    }
+}
