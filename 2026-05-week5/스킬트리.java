92c54e2168a98526709f0470a48c8a896ae8aa3f
rudals2334 <rudals831@gmail.com>
2026-05-29 17:52:12 +0900

    programmers: 스킬트리

--- 2026-05-week5/스킬트리.java
+++ 2026-05-week5/스킬트리.java
@@ -1,24 +1,16 @@
-import java.util.ArrayList;
-import java.util.List;
 class Solution {
     public int solution(String skill, String[] skill_trees) {
-        List<String> list = new ArrayList<>();
-        List<String> list2 = new ArrayList<>();
         int answer = 0;
-        for(int i = 0; i < skill.length(); i++){
-            String k = skill.substring(i, i+1);
-            list.add(k);
-            String u = skill.substring(0,i+1);
-            list2.add(u);
-        }
         for(String tree : skill_trees){
-            String a = "";
-            for(int i = 0; i < tree.length(); i++){
-                if(list.contains(tree.substring(i,i+1))){
-                    a = a+tree.substring(i,i+1);
+            StringBuilder sb = new StringBuilder();
+
+            for(char c : tree.toCharArray()){
+                if(skill.contains(String.valueOf(c))){
+                    sb.append(c);
                 }
             }
-            if(a.equals("") || list2.contains(a)){
+
+            if(skill.startsWith(sb.toString())){
                 answer++;
             }
         }
