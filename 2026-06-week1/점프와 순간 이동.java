66e686d8d17af99e12a0e16935cd94d8863e73cd
rudals2334 <rudals831@gmail.com>
2026-06-02 16:21:17 +0900

    programmers: 점프와 순간 이동

---
+++ 2026-06-week1/점프와 순간 이동.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        while(n!=0){
+            if(n%2==0){
+                n=n/2;
+            }else{
+                n-=1;
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
