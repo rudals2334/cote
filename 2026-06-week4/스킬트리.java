04648c0f5f8f640aee65b8b8f24aade197adb3c8
rudals2334 <rudals831@gmail.com>
2026-06-25 18:12:12 +0900

    스킬트리.java

---
+++ 2026-06-week4/스킬트리.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(String skill, String[] skill_trees) {
+        int answer = 0;
+        for(String tree : skill_trees){
+            StringBuilder sb = new StringBuilder();
+            for(char c : tree.toCharArray()){
+                if(skill.contains(String.valueOf(c))){
+                    sb.append(c);
+                }
+            }
+
+            if(skill.startsWith(sb.toString())){
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
