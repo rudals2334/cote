99e50724d2a9d52720786f0d18d7bf5ef1283985
rudals2334 <rudals831@gmail.com>
2026-07-30 17:42:14 +0900

    programmers: 최솟값 만들기

---
+++ 2026-07-week5/최솟값 만들기.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+class Solution
+{
+    public int solution(int[] A, int[] B){
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        int answer = 0;
+        for(int i = 0; i < A.length; i++){
+            int m = A[i] * B[A.length-1-i];
+            answer += m;
+        }
+        return answer;
+    }
+}
