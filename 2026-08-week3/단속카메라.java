df85ae699aeae61308ecc2a611e0d1e30d95b005
rudals2334 <rudals831@gmail.com>
2026-08-17 16:32:47 +0900

    programmers: 단속카메라

---
+++ 2026-08-week3/단속카메라.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+class Solution {
+    public int solution(int[][] routes) {
+        Arrays.sort(routes,(a,b) -> Integer.compare(a[1], b[1]));
+        int answer = 0;
+        int camera = Integer.MIN_VALUE;
+        for(int[] route : routes){
+            if(camera < route[0]){
+                camera = route[1];
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
