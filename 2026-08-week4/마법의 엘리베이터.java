ac876dc8c92cae96ce904f4235b0e34f78886412
rudals2334 <rudals831@gmail.com>
2026-08-24 17:55:01 +0900

    programmers: 마법의 엘리베이터

---
+++ 2026-08-week4/마법의 엘리베이터.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int solution(int storey){
+        int answer = 0;
+        while (storey != 0) {
+            int digit = storey % 10;
+            int next = (storey / 10) % 10;
+            if(digit > 5){
+                answer += (10 - digit);
+                storey += (10 - digit);
+                storey /= 10;
+            }else if(digit < 5){
+                answer += digit;
+                storey -= digit;
+                storey /= 10;
+            }else{
+                if(next >= 5){
+                    answer += (10-digit);
+                    storey += (10 - digit);
+                    storey /= 10;
+                }else{
+                    answer += digit;
+                    storey -= digit;
+                    storey /= 10;
+                }
+            }
+        }
+        return answer;
+    }
+}
