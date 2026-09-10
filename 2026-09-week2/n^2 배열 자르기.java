4ff774f4099d7d65ac23d58c0fec673a790d04d6
rudals2334 <rudals831@gmail.com>
2026-09-10 16:28:42 +0900

    n^2 베열 자르기

---
+++ 2026-09-week2/n^2 배열 자르기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] solution(int n, long left, long right) {
+        int[] answer = new int[(int)(right - left + 1)];
+
+        int a = 0;
+        for(long i = left; i <= right; i++){
+            long row = i / n;
+            long col = i % n;
+            int k = (int)Math.max(row, col) + 1;
+            answer[a] = k;
+            a++;
+        }
+
+        return answer;
+    }
+}
